37a8f095b75c75ecbf51e736431745238aa8520a
Alexander Desmond <AlexanderDesmond848898@gmail.com>
2019-11-14 15:26:11 +1100

    Create a grid and render it on the canvas.

---
+++ index.js
@@ -0,0 +1,38 @@
+// Initialise the canvas
+const CANVAS = document.querySelector("canvas");
+const CONTEXT = CANVAS.getContext("2d");
+
+// Set the resolution and dimensions of the canvas.
+const RESOLUTION = 40;
+CANVAS.width = 400;
+CANVAS.height = 400;
+
+// Set the number of rows and columns
+const COLUMNS = CANVAS.width / RESOLUTION;
+const ROWS = CANVAS.height / RESOLUTION;
+
+const GRID = buildGrid();
+render(GRID);
+
+// Return a two-dimensional array to hold the grid.
+function buildGrid() {
+  return new Array(COLUMNS).fill(null).map(() => new Array(ROWS).fill(0));
+}
+
+// Render the grid on the canvas.
+function render(grid) {
+  for (let column = 0; column < grid.length; column++) {
+    for (let row = 0; row < grid[column].length; row++) {
+      const cell = grid[column][row];
+
+      CONTEXT.beginPath();
+      CONTEXT.rect(
+        column * RESOLUTION,
+        row * RESOLUTION,
+        RESOLUTION,
+        RESOLUTION
+      );
+      CONTEXT.stroke();
+    }
+  }
+}
